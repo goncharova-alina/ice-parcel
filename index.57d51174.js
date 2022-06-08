ef4b31185cd19dc9af4706f4a87c82e6b972cc5f
goncharova-alina <62941341+goncharova-alina@users.noreply.github.com>
2022-06-08 15:02:31 +0000

    Deploying to gh-pages from @ goncharova-alina/ice-parcel@9d0b2bb25053d076a8ad9fe45460cb129ad1066b 🚀

---
+++ index.57d51174.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),backdrop:document.querySelector("[data-backdrop]")};function o(){e.backdrop.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o),e.backdrop.addEventListener("click",(function(){console.log("Это клик в бекдроп")}))}();
+//# sourceMappingURL=index.57d51174.js.map
